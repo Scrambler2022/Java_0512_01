831a8b485efa6c759ff8ac54817300cd8ee2303e
Scrambler2022 <93297236+Scrambler2022@users.noreply.github.com>
2023-05-13 17:58:21 +0900

    _00_Class_05.java

--- src/_00_Class_05.java
+++ src/_00_Class_05.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 public class _00_Class_05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        //int[][][][][] input = new int[0][1][2][3][4];
-        int[] num = new int[4];
+        //int [][][][] input = new int[0][2][3][4][5];
+        int[] num = new int[5];
         //int num =0;
         //
-        String[] data = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001"};
+        String[] data = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001",};
         //1. 숫자입력
-        for (int i = 0; i <5; i++) {
+        for (int i=0; i<5; i++) {
             System.out.println("10진수를 입력하세요");
             num[i] = scan.nextInt();
-            //num[i] = scan.nextInt();
-            //2진코드 출력
         }
         for (int i=0; i<5; i++) {
             System.out.println(data[num[i]]+" ");
-        }
+        }//for
     }//main
 }//class
