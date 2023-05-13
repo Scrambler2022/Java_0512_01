b11bd2247b1c3d1bcbcea77ec884696d1fc4828b
Scrambler2022 <93297236+Scrambler2022@users.noreply.github.com>
2023-05-13 18:13:50 +0900

    _00_Class_06.java

--- src/_00_Class_06.java
+++ src/_00_Class_06.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class _00_Class_06 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-//        int[] num = new int[3];
-//        for (int i=0; i< num.length;i++) {
-//            System.out.println("점수를 입력하세요");
-//            num[i] = scan.nextInt();
-//        }//for
+
+        // 3행 4열의 2차원 배열 선언
         int[][] score = new int[3][4];
-        //
-        for (int i= 0; i<score.length;i++) {
-            for (int j=0; j< score[i].length; j++) {
+
+        // 배열의 각 요소에 값을 입력
+        for (int i=0; i<score.length; i++) { // 각 행에 대해서
+            for (int j=0; j<score[i].length; j++) { // 각 열에 대해서
                 System.out.println("2차원 점수를 입력하세요");
                 score[i][j] = scan.nextInt();
             }
         }
-        //출력
-        for (int i=0;i< score.length;i++) {
-            for (int j=0;i< score.length;j++)
-            System.out.println(score[i][j]+" ");
+
+        // 배열의 각 요소를 출력
+        for (int i=0; i<score.length; i++) { // 각 행에 대해서
+            for (int j=0; j<score[i].length; j++) {
+                System.out.print(score[i][j]+" ");
+            }
+            System.out.println();
         }
-        System.out.println();
-    }//main
+    }
 }
